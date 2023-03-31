0a2067f53d4667f2638b9a74d76912fa05b8c39d
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-03-31 11:41:28 -0300

    added hamburger menu to the style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import "./NavbarStyles.css";
 
+//import dependencies
+import React, {useState} from 'react';
 
-import React from 'react';
 import { Link } from "react-router-dom";
 
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+
+  //function to set click for hamburger menu//
+    const [click, onClick]=useState(false);
+    const selectClick = () => onClick(!click);
     return (
         <div className="header">
            <Link to="/">
              <h1>Portfolio</h1>
            </Link>
-           <ul className="nav-list">
+           <ul className={click ? "nav-list active" : 
+            "nav-list"}>
               <li>
                 <Link to="/">Home</Link>
               </li>
@@ -24,8 +32,15 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>
               </li>
            </ul>
+           <div className="hamburger" onClick={selectClick}> 
+              {click ? 
+             ( <FaTimes size={20} style={{color: "white"}}/>):
+
+             ( <FaBars size={20} style={{color: "white"}}/>)
+              }
+           </div>
         </div>
     );
 };
 
-export default Navbar
+export default Navbar;
