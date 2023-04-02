f7a6fc34d183fd83800fe1c78a06e9f968bad177
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-01 23:51:36 -0300

    added styles and icons to footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,29 @@
-import "./Footer.css"
+import "./FooterStyles.css";
 
-import React from 'react'
+import React from 'react';
+//import icons
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
-const footer = () => {
+
+const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer-container">
-         
-        </div>
+        <div className="social">
+            <FaGithub
+                size={30}
+                style={{ color: "#fff",
+                marginRight: "1rem"}}/>
+            <FaLinkedin
+                size={30}
+                style={{ color: "#fff",
+                marginRight: "1rem"}}/>
+            <FaTwitter
+                size={30}
+                style={{ color: "#fff",
+              marginRight: "1rem"}}/>
+        </div>  
     </div>
   )
 }
 
-export default footer
+export default Footer
