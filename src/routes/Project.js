c694c1081869129b6c34e206a50f58e7866f521c
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-02 00:58:00 -0300

    added a function to parse and render props in project page

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Hero from "../components/Hero";
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
+      <Hero heading="PORTFOLIO." text="Some of the projects I have worked on"/>
       <Footer />    
     </div>
   );
